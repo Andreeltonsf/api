6e91278fd1f0a5671869a4b99dfc08282c7357fd
Francisco Ferreira <franciscoandreeltonsf@gmail.com>
2025-05-02 10:53:18 -0300

    fix: update import path for UsersModule and add PrismaService to providers

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismaService } from 'src/shared/database/prisma.service';
 import { UsersRepositories } from 'src/shared/database/repositories/users.repositories';
 import { UsersController } from './users.controller';
 import { UsersService } from './users.service';
 
 @Module({
   controllers: [UsersController],
-  providers: [UsersService, UsersRepositories],
+  providers: [UsersService, UsersRepositories, PrismaService],
 })
 export class UsersModule {}
