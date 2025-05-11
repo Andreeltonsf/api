38d6b2fecc449a74125cb4d347adfb6ebc8f45ff
Francisco Ferreira <franciscoandreeltonsf@gmail.com>
2025-05-11 16:56:47 -0300

    feat: add public metadata to signin and signup endpoints and enhance AuthGuard to check for public routes

--- src/modules/auth/auth.guard.ts
+++ src/modules/auth/auth.guard.ts
@@ -10,13 +10,25 @@ import { JwtService } from '@nestjs/jwt';
 import 'dotenv/config';
 import { env } from 'src/shared/config/env';
 
+import { Reflector } from '@nestjs/core';
 import { Request } from 'express';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService) {}
+  constructor(
+    private jwtService: JwtService,
+    private reflector: Reflector,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.getAllAndOverride<boolean>('IS_PUBLIC', [
+      context.getClass(),
+      context.getHandler(),
+    ]);
+
+    if (isPublic) {
+      return true;
+    }
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
     if (!token) {
