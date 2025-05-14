1bbb0ef9695eec6a5caab441f63c364d9b7fd959
Francisco Ferreira <franciscoandreeltonsf@gmail.com>
2025-05-14 15:33:24 -0300

    feat: enhance bank accounts service to calculate current balance with transaction details

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,18 @@
-import 'dotenv/config';
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import 'dotenv/config';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.useGlobalPipes(new ValidationPipe());
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,PUT,POST,DELETE',
+    preflightContinue: false,
+    optionsSuccessStatus: 204,
+  });
 
   await app.listen(process.env.PORT ?? 3000);
 }
